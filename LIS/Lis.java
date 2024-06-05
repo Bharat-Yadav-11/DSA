eb14300cce16fc65fc491f8e5daf116080d6cf9a
Bharat <142567000+Bharat-Yadav-11@users.noreply.github.com>
2024-06-05 13:12:25 +0000

    update

--- LIS/Lis.java
+++ LIS/Lis.java
@@ -2,7 +2,6 @@ package LIS;
 
 class LIS {
 
-    
     static int lis(int arr[], int n) {
         int lis[] = new int[n];
         int i, j, max = 0;
@@ -11,7 +10,6 @@ class LIS {
         for (i = 0; i < n; i++)
             lis[i] = 1;
 
-        
         for (i = 1; i < n; i++)
             for (j = 0; j < i; j++)
                 if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
